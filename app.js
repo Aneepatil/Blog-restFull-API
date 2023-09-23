39ea0ef5680f5243bf6ae13ff9134b092970c6ee
Aneepatil <anilspatil8@gmail.com>
2023-09-23 12:45:41 +0530

    changes

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from 'cors'
 import blogRoute from "./routes/blogRoute.js";
 import { dbConnect } from "./config/dbConnect.js";
 import userRoute from "./routes/userRoute.js";
@@ -7,6 +8,7 @@ const app = express();
 
 // Middlewares
 app.use(express.json());
+app.use(cors())
 
 // Data Base Config
 dbConnect(app);
